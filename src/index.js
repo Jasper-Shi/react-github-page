9d70f0085f0e1bb667b04dd8d14c66188c55629f
Jasper-Shi <31856111+Jasper-Shi@users.noreply.github.com>
2018-04-02 13:24:59 -0400

    routing1

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render((
-    <BrowserRouter>
+ReactDOM.render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
-  ),document.getElementById('root'));
+  ,document.getElementById('root'));
